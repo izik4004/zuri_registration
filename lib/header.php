06a26a1f0cdfdc778e372d74096ce324a77e68f9
unknown <izik4004>
2021-04-16 09:54:47 +0100

    made some changes

--- lib/header.php
+++ lib/header.php
@@ -15,7 +15,6 @@
 <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
         <h5 class="my-0 mr-md-auto font-weight-normal"><a href="index.php">Zuri</a></h5>
         <nav class="my-2 my-md-0 mr-md-3">
-            <a class="p-2 text-dark" href="index.php">Home</a>
             <?php if(!isset($_SESSION['loggedIn'])){ ?>
         
                 <a class="p-2 text-dark" href="login.php">Login</a> 
